16f8bc5900dc75962ee012bfeba993ab692e504a
John Kang <pollyfactor@hotmail.com>
2020-03-11 09:09:20 -0400

    updated index.html

--- script/questions.js
+++ script/questions.js
@@ -1,27 +1,34 @@
 var questions = [
-    {
-        question: "Commonly used data tyoes DO NOT include:",
-        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
-        answer: "Alerts"
-    },
-    {
-        question: "The condition in an if/else statement is enclosed within:",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
-    },
-    {
-        question: "Arrays in Javascript can be used to store:",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: ""
-    },
-    {
-        question: "String values must be enclosed within _____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: "quotes"
-    },
-    {
-        question: "A very useful tool used dureing development and debugging for printing content to the debugger is:",
-        choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
-        answer: "console.log"
-    },
+  {
+    question: "Commonly used data tyoes DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "lerts"
+  },
+  {
+    question: "The condition in an if/else statement is enclosed within:",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    question: "Arrays in Javascript can be used to store:",
+    choices: [
+      "numbers and strings",
+      "other arrays",
+      "booleans",
+      "all of the above"
+    ],
+    answer: "all of the above"
+  },
+  {
+    question:
+      "String values must be enclosed within _____ when being assigned to variables.",
+    choices: ["commas", "curly brackets", "quotes", "parentheses"],
+    answer: "quotes"
+  },
+  {
+    question:
+      "A very useful tool used dureing development and debugging for printing content to the debugger is:",
+    choices: ["Javascript", "terminal/bash", "for loops", "console.log"],
+    answer: "console.log"
+  }
 ];
