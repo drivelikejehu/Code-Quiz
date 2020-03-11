1d5d6d9e6a4b840e79c1ed40a7c68ab10abe7582
John Kang <pollyfactor@hotmail.com>
2020-03-11 17:10:40 -0400

    updated script.js

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
 var startBtn = document.getElementById("startBtn");
 var timerEl = document.getElementById("timer");
 
+startBtn.addEventListener("click",function() {
+    document.getElementById("main").classList.add("d-none");
+    document.getElementById("quiz").classList.remove("d-none");
+});
