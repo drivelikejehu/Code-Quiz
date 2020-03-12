d8d1202a93370ddc1f9aff58e3245cfa261f7454
John Kang <pollyfactor@hotmail.com>
2020-03-11 23:23:47 -0400

    updated script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,11 @@
 var startBtn = document.getElementById("startBtn");
 var timerEl = document.getElementById("timer");
 var timerSeconds = 70;
+var index = 0;
 var questionsEl = document.getElementById("questions");
+var choicesEl = document.getElementById("choices");
+
+// start of quiz
 
 startBtn.addEventListener("click", function() {
     document.getElementById("main").classList.add("d-none");
@@ -10,18 +14,32 @@ startBtn.addEventListener("click", function() {
     startQuestions();
 });
 
+// start of timer 
 function setTime() {
 
     var counter = setInterval(function () {
         timerSeconds--;
-        timerEl.innerHTML = "Timer: " + timerSeconds;
+        timerEl.textContent = "Timer: " + timerSeconds;
 
         if (timerSeconds == -1){
             clearInterval(counter);
         }
     }, 1000);
+    // quizFinal();
 }
 
+// making questions
 function startQuestions() {
-    
-};
+
+    questionsEl.textContent = questions[index].question;
+    choicesEl.innerHTML = "";
+
+    var choices = questions[index].choices;
+
+    for (var i = 0; i < choices.length; i++) {
+        var choiceButton = document.createElement("button");
+
+        choiceButton.textContent = choices[i];
+        choicesEl.appendChild(choiceButton).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+    }
+}
