4280461d88734ad3b408ed590f10a22561ee4eb7
John Kang <pollyfactor@hotmail.com>
2020-06-04 19:31:44 -0400

    created startQuiz function eventlistener

--- script/script.js
+++ script/script.js
@@ -1,45 +1,48 @@
-var startBtn = document.getElementById("startBtn");
-var timerEl = document.getElementById("timer");
+const startBtn = document.getElementById("startBtn");
+const timerEl = document.getElementById("timer");
 var timerSeconds = 70;
 var index = 0;
 var questionsEl = document.getElementById("questions");
 var choicesEl = document.getElementById("choices");
 
-// start of quiz
+// startBtn.addEventListener("click", function () {
+//   document.getElementById("main").classList.add("d-none");
+//   document.getElementById("quiz").classList.remove("d-none");
+//   setTime();
+//   startQuestions();
+// });
 
-startBtn.addEventListener("click", function() {
+startBtn.addEventListener('click', startQuiz)
+
+function startQuiz() {
     document.getElementById("main").classList.add("d-none");
     document.getElementById("quiz").classList.remove("d-none");
-    setTime();
+    setTimer();
     startQuestions();
-});
-
-// start of timer 
-function setTime() {
-
-    var counter = setInterval(function () {
-        timerSeconds--;
-        timerEl.textContent = "Timer: " + timerSeconds;
-
-        if (timerSeconds == -1){
-            clearInterval(counter);
-        }
-    }, 1000);
-    // quizFinal();
 }
 
-// making questions
-function startQuestions() {
+function setTimer() {
+  var counter = setInterval(function () {
+    timerSeconds--;
+    timerEl.textContent = "Timer: " + timerSeconds;
 
-    questionsEl.textContent = questions[index].question;
-    choicesEl.innerHTML = "";
-
-    var choices = questions[index].choices;
-
-    for (var i = 0; i < choices.length; i++) {
-        var choiceButton = document.createElement("button");
-
-        choiceButton.textContent = choices[i];
-        choicesEl.appendChild(choiceButton).setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+    if (timerSeconds == -1) {
+      clearInterval(counter);
     }
+  }, 1000);
+}
+
+function startQuestions() {
+  questionsEl.textContent = questions[index].question;
+  choicesEl.innerHTML = "";
+
+  var choices = questions[index].choices;
+
+  for (var i = 0; i < choices.length; i++) {
+    var choiceButton = document.createElement("button");
+    choiceButton.textContent = choices[i];
+    choicesEl
+      .appendChild(choiceButton)
+      .setAttribute("class", "p-3 m-1 btn btn-light btn-block");
+  }
 }
